ac760832be9156640cd0b589f9e7ae001ce598f8
liangzhi <liangzhi@topeastic.com>
2016-03-17 17:08:01 +0800

    first comit
    Signed-off-by: liangzhi <liangzhi@topeastic.com>

---
+++ nineimagegroupview/src/main/java/com/lazh/nineimagegroupview/NineImageAdapter.java
@@ -0,0 +1,15 @@
+package com.lazh.nineimagegroup;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by Lazh on 2016/3/17.
+ */
+public abstract class NineImageAdapter {
+    public ImageView initImageView(Context context){
+        ImageView imageView = new ImageView(context);
+        return imageView ;
+    }
+    public abstract void  displayImageView(Context context,ImageView imageView , int position, String url);
+}
